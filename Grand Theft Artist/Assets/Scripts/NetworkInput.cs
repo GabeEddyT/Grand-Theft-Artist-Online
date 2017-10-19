d086275a440ce0c69524490961d9b0574b3200f2
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2017-10-19 02:30:11 -0400

    Debugging translation from C# to C

--- Grand Theft Artist/Assets/Scripts/NetworkInput.cs
+++ Grand Theft Artist/Assets/Scripts/NetworkInput.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
+
 
 public class NetworkInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [DllImport("MyFrameworkPlugin")]
+    static extern int Startup();
+
+    [DllImport("MyFrameworkPlugin")]
+    static extern unsafe char* CStringTest();
+
+    [DllImport("MyFrameworkPlugin")]
+    static extern unsafe char* InputTest(char* stuff);
+
+    // Use this for initialization
+
+
+
+    void Start () {
+        debugCstring();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    unsafe void debugCstring()
+    {
+        string str = Marshal.PtrToStringAnsi((System.IntPtr)CStringTest());
+            Debug.Log(str);
+        string mama = "hey hello there";
+        fixed (char* p = mama)
+        {
+            Marshal.PtrToStringAnsi((System.IntPtr)InputTest(p));
+            Debug.Log(mama);
+
+        }
+    }
+
 }
