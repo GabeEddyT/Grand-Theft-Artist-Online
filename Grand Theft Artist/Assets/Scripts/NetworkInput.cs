e5790e6c7ad72c5118e117a634cf7619d9add1e1
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2017-12-04 10:08:04 -0500

    Remove old debug methods

--- Grand Theft Artist/Assets/Scripts/NetworkInput.cs
+++ Grand Theft Artist/Assets/Scripts/NetworkInput.cs
@@ -61,7 +61,7 @@ public class NetworkInput : MonoBehaviour {
     public static bool networkedMode = false;
 
 
-    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 81)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public unsafe struct GameState
     {
         public byte id;
@@ -146,60 +146,6 @@ public class NetworkInput : MonoBehaviour {
         //{
         //    SendInput();
         //}
-    }
-
-    //unsafe void debugCstring()
-    //{
-    //    string str = Marshal.PtrToStringAnsi((System.IntPtr)CStringTest());
-    //        Debug.Log(str);
-    //    string mama = "hello there";
-    //    System.Text.StringBuilder tester = new System.Text.StringBuilder(mama);
-    //    // char* the;
-    //    string pStr = "Hello World!";
-
-    //    InputMessage* pChars = (InputMessage*)Marshal.StringToHGlobalAnsi(pStr).ToPointer();
-    //    char* pSomething = (char*)pChars;
-    //    char* charArray;
-    //    InputMessage* pTerrible;
-    //    InputMessage* pTulips = (InputMessage*)Marshal.StringToHGlobalAnsi(new string(pSomething)).ToPointer();
-
-    //    fixed (char* p = "hey hello yada")
-    //    {
-    //        //string derp = Marshal.PtrToStringAnsi((System.IntPtr)(p));
-    //        string derp = new string(p);
-    //        Debug.Log(new string(p));
-
-    //    }
-
-
-    //}
-
-    unsafe void debugStruct()
-    {
-        InputMessage tom = (InputMessage) Marshal.PtrToStructure(getTest(), typeof( InputMessage)); //this works :)
-        
-        Debug.Log("tom's id: " + tom.id);
-
-        Debug.Log("tom's horizontal: " + tom.horizontal);
-        Debug.Log("tom's vertical: " + tom.vertical);
-
-
-    }
-
-    void SendAndReceiveStruct()
-    {
-        InputMessage lambda = new InputMessage();
-        lambda.vertical = 115.7f;
-        lambda.horizontal = 38.129f;
-
-        IntPtr kappa = Marshal.AllocHGlobal(Marshal.SizeOf(lambda));
-        
-        Marshal.StructureToPtr(lambda, kappa, false);
-        
-        kappa = returnToSender(kappa);
-        
-        InputMessage backHome = (InputMessage)Marshal.PtrToStructure(kappa, typeof(InputMessage));
-        Debug.Log("Horizontal: " + backHome.horizontal + " Vertical: " + backHome.vertical + " ID: " + backHome.id);
     }
 
     public void ConnectToRakNet()
