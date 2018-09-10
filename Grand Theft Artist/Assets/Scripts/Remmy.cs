ae556cb4602a85760349a377c3ebdfac4f30c702
Gabe Troyan <gabriel.troyan@mymail.champlain.edu>
2018-09-10 18:05:25 -0400

    Try out navmesh

---
+++ Grand Theft Artist/Assets/Scripts/Remmy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Remmy : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(new Vector3(-212.44f, 56f));
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("speed", agent.velocity.magnitude / 7);
+    }
+}
